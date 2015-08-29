d29875069882872e51fbc5625142800e9acae071
webb04 <jamie.webb@warwick.ac.uk>
2015-08-29 21:32:22 +0100

    make it better

---
+++ getting-started.js
@@ -0,0 +1,43 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/myappdatabase');
+
+
+// create a schema
+var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// create a new user
+var newUser = User({
+  name: '1',
+  username: '2',
+  password: '3',
+  admin: true
+});
+
+// save the user
+newUser.save(function(err) {
+  if (err) throw err;
+
+  console.log('User created!');
+});
+
+// make this available to our users in our Node applications
+module.exports = User;
